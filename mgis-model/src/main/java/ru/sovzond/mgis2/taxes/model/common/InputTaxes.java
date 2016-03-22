b77ffeff4bc4cfa32ef89f553b93cb9ef57b6113
Sergey Lvov <slvov@sovzond.ru>
2016-03-22 16:33:03 +0300

    Refactoring: taxes.

--- mgis-model/src/main/java/ru/sovzond/mgis2/taxes/model/common/InputTaxes.java
+++ mgis-model/src/main/java/ru/sovzond/mgis2/taxes/model/common/InputTaxes.java
@@ -1,13 +1,10 @@
 package ru.sovzond.mgis2.taxes.model.common;
 
-import com.sun.istack.internal.NotNull;
-
 import javax.persistence.*;
 import java.util.Date;
 
 /**
  * Created by donchenko-y on 3/22/16.
- *
  */
 
 @Entity
@@ -23,8 +20,8 @@ public class InputTaxes implements Cloneable {
 	/**
 	 * Кадастровый номер ЗУ
 	 */
-	@NotNull
-	@Column
+
+	@Column(nullable = false)
 	private String cadastralNumber;
 
 	/**
